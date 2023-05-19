4da5202818b55fe2dfb999875a5b5ce210942e7d
root <root@ip-172-31-14-129.ap-south-1.compute.internal>
2023-05-19 23:08:36 +0000

    added all files

---
+++ before_install.sh
@@ -0,0 +1,7 @@
+yum install docker -y
+service docker start
+sudo usermod -a -G docker jenkins
+https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+
